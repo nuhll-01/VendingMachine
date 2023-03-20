64c908c0b123562fc59b2040e945179e9f82a00c
ToastyX <erick.cool321@icloud.com>
2023-03-19 22:41:04 -0500

    5th commit: Added new functionality and features.

--- main.cpp
+++ main.cpp
@@ -1,54 +1,85 @@
-// Test Commit
 #include <iostream>
-#include <cmath>
+#include <chrono>
+#include <thread>
 #include <string>
 
 class VendingMachine {
 
-    std::string snacks[7] = {"Starburst", "Skittles", "Takis", "Welch Berry Gummy",
-                         "Trolli Sour Worms", "Hot Cheetos", "Water"};
+private:
+    int code{};
+    double currentBalance{};
+    double money{};
+    std::string userInput{};
 
 public:
-    double currentBalance;
-    double money;
-
     void menu() {
-
-        std::string userContinue;
-        std::cout << "Starburst - (001)" << " ||" << " Skittles - (002)" << " ||" << " Takis - (003)"
-             << " ||" << " Welch Berry Gummy - (004)" << " ||" << " Trolli Sour Worms - (005)" << std::endl;
+        items();
         deposit();
-        std::cout << "Current Balance: " << "$" << balance(currentBalance) << std::endl;
+        std::cout << "\nCurrent Balance: " << "$" << balance(currentBalance) << std::endl;
         std::cout << "Insert more cash? (Yes || No): ";
-        std::cin >> userContinue;
-        while(userContinue == "yes" || userContinue == "Yes") {
+        std::cin >> userInput;
+        while (userInput == "yes" || userInput == "Yes") {
             deposit();
-            std::cout << "Current Balance: " << "$" << balance(currentBalance) << std::endl;
+            std::cout << "\nCurrent Balance: " << "$" << balance(currentBalance) << std::endl;
             if (balance(currentBalance) >= 5.00) {
                 std::cout << "Limit Reached." << std::endl;
                 break;
             } else {
                 std::cout << "Insert Cash? (Yes || No): ";
-                std::cin >> userContinue;
+                std::cin >> userInput;
             }
         }
-        std::cout << "\nEnter Selection Code: " << std::endl;
+        std::cout << "\nEnter Code: ";
+        std::cin >> code;
+        getItem(code);
     }
 
+private:
     // Starting Function
     void deposit() {
         double cash;
-        std::cout << "Insert Cash ($1.25): ";
+        std::cout << "\n\nInsert Cash (Accepts $1 and $5): ";
         std::cin >> cash;
-        if (cash >= 5.00) {
-            std::cout << "Limit Reached." << std::endl;
+        if (cash == 1.00) {
+            currentBalance++;
+        } else {
+            printf("%s", "Invalid Currency.");
         }
-        currentBalance = currentBalance + cash;
     }
 
-    // Users Current Balance
+    // Users current balance
     double balance(double userMoney) {
         return money + userMoney;
+    }
+
+    // the current list of items
+    static void items() {
+        printf("%s", "Starburst - $1.25 (001)"
+                     "\nSkittles - $1.25 (002)"
+                     "\nTrolli Sour Octopuses - $1.50 (003)"
+                     "\nWelch's Fruit Snacks - $1.50 (004)"
+                     "\nGatorade - $1.75 (005)");
+    }
+
+    static void getItem(int numbCode) {
+        int userCode = numbCode;
+        std::string snacks[6] = {"Starburst", "Skittles", "Welch Berry Gummy",
+                                 "Trolli Sour Octopuses", "Hot Cheetos", "Gatorade"};
+
+        if (userCode == 001) {
+            printf("%s", "Processing Order...");
+            sleep();
+            std::cout << "\nItem Chosen:" << " " << snacks[0] << std::endl;
+        }
+    }
+
+    // Delays execution.
+    static void sleep() {
+        using namespace std::this_thread; // sleep_for, sleep_until
+        using namespace std::chrono; // nanoseconds, system_clock, seconds
+
+        sleep_for(nanoseconds(10));
+        sleep_until(system_clock::now() + seconds(1));
     }
 };
 
