4c5254ce7be0c50539bfe3bf74f192ed47df0b61
ToastyX <erick.cool321@icloud.com>
2023-03-21 22:56:12 -0500

    7th commit: New functionality and code refinement

--- main.cpp
+++ main.cpp
@@ -6,9 +6,8 @@
 class VendingMachine {
 
 private:
-    int code{};
-    double currentBalance{};
-    double money{};
+    int code{0};
+    double currentBalance{0.0}, money{0.0};
     std::string userInput{};
 
 public:
@@ -16,14 +15,14 @@ public:
         items();
         for (int i = 0; i < 2; i++) {
             deposit();
-            std::cout << "\nCurrent Balance: " << "$" << balance(currentBalance) << std::endl;
+            std::cout << "\nCurrent Balance: " << "$" << balance(currentBalance) << '\n';
         }
         std::cout << "\nInsert more cash? (Yes || No): ";
         std::cin >> userInput;
         while (userInput == "yes" || userInput == "Yes") {
             deposit();
-            std::cout << "\nCurrent Balance: " << "$" << balance(currentBalance) << std::endl;
-            if (balance(currentBalance) >= 5.00) {
+            std::cout << "\nCurrent Balance: " << "$" << balance(currentBalance) << '\n';
+            if (balance(currentBalance) == 5.00) {
                 printf("\n%s", "Limit Reached");
                 break;
             } else {
@@ -42,10 +41,11 @@ private:
         double cash;
         std::cout << "\nInsert Cash (Accepts $1 and $5): ";
         std::cin >> cash;
+        isCurrency(cash);
         if (cash == 1.00) {
             currentBalance++;
         } else {
-            printf("%s", "Invalid Currency.");
+            printf("\n%s", "Invalid Currency.");
         }
     }
 
@@ -54,27 +54,41 @@ private:
         return money + userMoney;
     }
 
-    // the current list of items
+    // The list of items
     static void items() {
-        printf("%s", "Starburst - $1.25 (001)"
-                     "\nSkittles - $1.25 (002)"
-                     "\nTrolli Sour Octopuses - $1.50 (003)"
-                     "\nWelch's Fruit Snacks - $1.50 (004)"
-                     "\nGatorade - $1.75 (005)"
+        printf("%s", "Starburst - $1.25 (100)"
+                     "\nSkittles - $1.25 (101)"
+                     "\nWelch's Fruit Snacks - $1.50 (102)"
+                     "\nTrolli Sour Octopuses - $1.50 (103)"
+                     "\nHot Cheetos - $1.50 (104)"
+                     "\nGatorade - $1.75 (105)"
                      "\n");
     }
 
+    // Retrieves the item from the vending machine
     static void getItem(int numbCode) {
         int userCode = numbCode;
         std::string snacks[6] = {"Starburst", "Skittles", "Welch Berry Gummy",
                                  "Trolli Sour Octopuses", "Hot Cheetos", "Gatorade"};
 
-        if (userCode == 001) {
-            printf("\n%s", "Processing Order...");
-            sleep();
-            printf("\n%s", "Transaction Complete!");
-            sleep();
-            std::cout << "\nChosen item:" << " " << snacks[0] << std::endl;
+        if (userCode == 100) {
+            processOrder();
+            std::cout << "\nChosen item:" << " " << snacks[0] << '\n'; // Starburst
+        } else if (userCode == 101) {
+            processOrder();
+            std::cout << "\nChosen item:" << " " << snacks[1] << '\n'; // Skittles
+        } else if (userCode == 102) {
+            processOrder();
+            std::cout << "\nChosen item:" << " " << snacks[2] << '\n'; // Welch's
+        } else if (userCode == 103) {
+            processOrder();
+            std::cout << "\nChosen item:" << " " << snacks[3] << '\n'; // Trolli
+        } else if (userCode == 104) {
+            processOrder();
+            std::cout << "\nChosen item:" << " " << snacks[4] << '\n'; // Hot Cheetos
+        } else if (userCode == 105) {
+            processOrder();
+            std::cout << "\nChosen item:" << " " << snacks[5] << '\n'; // Gatorade
         }
     }
 
@@ -85,6 +99,22 @@ private:
 
         sleep_for(nanoseconds(10));
         sleep_until(system_clock::now() + seconds(1));
+    }
+
+    static void processOrder() {
+        printf("\n%s", "Processing Order...");
+        sleep();
+        printf("%s", "Transaction Complete!");
+        sleep();
+    }
+
+    // (WORK-IN-PROGRESS) //
+    static bool isCurrency(double cash) {
+        double currency{};
+        if (cash < currency) {
+            return "\ninvalid currency";
+        }
+        return false;
     }
 };
 
